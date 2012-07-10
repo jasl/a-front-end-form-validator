e421678a9fffa563ea7a9f3fc1b45896c9d32f7a
Jasl <jasl9187@hotmail.com>
2012-07-10 16:06:58 +0800

    add specific callback add triggle condition refactor near all code just have basic test

--- README.markdown
+++ README.markdown
@@ -1,1 +1,7 @@
-just see sample.html :)
+#A-front-end-form-validator
+
+A rails-style javascript form validator, not required other libs, with high customable.
+
+See sample.html to learn how to use :)
+
+*Copyright jasl, MIT licence.*
--- validator.js
+++ validator.js
@@ -1,34 +1,34 @@
 /**
  * @author Jasl
- * @version 0.6
+ * @version 0.7
  *
  */
 var Validator = (function() {
   var self = this;
 
-  var _default_message = '{alias}输入有误。';
-
   var _val_items = [];
 
   var _val_rules = [];
+
+  self.default_message = '{alias}输入有误。';
 
   var _final_message = function(message, item) {
     var msg = message;
     var variables = msg.match(/\{[.\w]+\}/ig);
 
-    if(msg.indexOf('{alias}') >= 0) {
+    if (msg.indexOf('{alias}') >= 0) {
       msg = msg.replace('{alias}', item.alias);
     }
 
-    for(var i in variables) {
+    for (var i in variables) {
       var key = variables[i].match(/\w+/ig);
       var value = item.pattern[key[0]];
-      if(key.length > 1) {
-        for(var v = 1; v < key.length; v++) {
+      if (key.length > 1) {
+        for (var v = 1; v < key.length; v++) {
           value = value[key[v]];
         }
       }
-      if( typeof (value) === 'object') {
+      if ( typeof (value) === 'object') {
         value = value.toString();
       }
       msg = msg.replace(variables[i], value);
@@ -38,7 +38,7 @@ var Validator = (function() {
   };
 
   self.add_rule = function(name, pattern) {
-    if(name !== '' && pattern && pattern.shoulda) {
+    if (name !== '' && pattern && pattern.shoulda) {
       _val_rules[name] = pattern;
       return true;
     } else {
@@ -48,73 +48,92 @@ var Validator = (function() {
 
   self.get_error_items = function() {
     var error_items = Array();
-    for(var i in _val_items) {
-      if(!_val_items[i].corrected) {
+    for (var i in _val_items) {
+      if (!_val_items[i].is_correct) {
         error_items.push(_val_items[i]);
       }
     }
     return error_items;
   };
 
-  self.callback = undefined;
+  self.default_callback = undefined;
 
-  self.summit_callback = undefined;
+  self.submit_callback = undefined;
 
-  self.ensure = function(item, pattern) {
-    for(var i in item) {
-      _val_items[i] = {
-        id : i,
-        alias : item[i],
-        pattern : pattern,
-        corrected : false
-      };
+  self.ensure = function(item, pattern, callback) {
+    var val_item = {
+      source : item.source,
+      pattern : pattern
+    };
+    if (item.alias !== undefined) {
+      val_item.alias = item.alias;
     }
+    if (item.condition !== undefined) {
+      val_item.condition = item.condition;
+    }
+    if (callback !== undefined) {
+      val_item.callback = callback;
+    }
+    _val_items.push(val_item);
   };
 
-  self.do_validate = function(item, value, use_callback) {
-    var pattern = item.pattern;
+  self.do_validate = function(item, use_callback) {
+    item.is_correct = true;
+    item.error_messages = [];
 
-    for(var i = 0; i < pattern.validates.length; i++) {
-      var rule = _val_rules[pattern.validates[i]];
-      if(rule) {
-        if(rule.preprocessing) {
+    if (!item.condition || item.condition()) {
+      var pattern = item.pattern;
+      for (var i in pattern.validates) {
+        var rule = _val_rules[pattern.validates[i]];
+        if (rule === undefined) {
+          continue;
+        }
+
+        if (rule.preprocessing) {
           rule.preprocessing(pattern);
         }
 
-        item.corrected = rule.shoulda(value, pattern);
-        if(!item.corrected) {
-          if(!pattern.message) {
-            item.message = _final_message(rule.message, item);
-          } else if(pattern.message) {
-            item.message = _final_message(pattern.message, item);
+        var value = null;
+        if ( typeof (item.source) === "string") {
+          value = window.document.getElementById(item.source).value.trim();
+        } else if ( typeof (item.source) === "function") {
+          value = item.source();
+        }
+        if (!rule.shoulda(value, pattern)) {
+          item.is_correct = false;
+          if (pattern.message) {
+            item.error_messages.push(_final_message(pattern.message, item));
+          } else if (rule.message) {
+            item.error_messages.push(_final_message(rule.message, item));
           } else {
-            item.message = _final_message(_default_message, item);
+            item.error_messages.push(_final_message(self.default_message, item));
           }
         }
       }
     }
 
-    if(use_callback && self.callback) {
-      self.callback(item, value);
+    if (use_callback) {
+      if (item.callback) {
+        item.callback(item, value);
+      } else if (self.default_callback) {
+        self.default_callback(item, value);
+      }
     }
 
-    return item.corrected;
+    return item.is_correct;
   };
 
   self.check_form = function() {
     var flag = true;
-    for(var i in _val_items) {
-      var tar = window.document.getElementById(_val_items[i].id);
-      if(tar) {
-        flag = self.do_validate(_val_items[i], tar.value.trim()) && flag;
-      }
+    for (var i in _val_items) {
+      flag = self.do_validate(_val_items[i]) && flag;
     }
 
-    if(!flag) {
-      if(self.summit_callback) {
-        self.summit_callback();
-      }
+    if (self.submit_callback) {
+      self.submit_callback();
+    }
 
+    if (!flag) {
       return false;
     } else {
       return true;
@@ -123,7 +142,14 @@ var Validator = (function() {
 
   self.check = function(e) {
     var event = arguments[0] || window.event;
-    return self.do_validate(_val_items[event.id], event.value, true);
+    var item = null;
+    for (var i in _val_items) {
+      if (_val_items[i].source === event.id) {
+        item = _val_items[i];
+        break;
+      }
+    }
+    return self.do_validate(item, true);
   };
 
   return self;
@@ -138,10 +164,10 @@ Validator.add_rule('presence', {
 
 Validator.add_rule('size', {
   preprocessing : function(pattern) {
-    if(!pattern.size.minimium) {
+    if (!pattern.size.minimium) {
       pattern.size.minimium = 0;
     }
-    if(!pattern.size.maximium) {
+    if (!pattern.size.maximium) {
       pattern.size.maximium = 50;
     }
   },
@@ -153,7 +179,7 @@ Validator.add_rule('size', {
 
 Validator.add_rule('format', {
   shoulda : function(value, pattern) {
-    if(!pattern.format) {
+    if (!pattern.format) {
       return false;
     }
     return pattern.format.test(value);
@@ -163,7 +189,7 @@ Validator.add_rule('format', {
 
 Validator.add_rule('shoulda', {
   shoulda : function(value, pattern) {
-    if(!pattern.shoulda) {
+    if (!pattern.shoulda) {
       return false;
     }
     return pattern.shoulda(value);
@@ -173,13 +199,16 @@ Validator.add_rule('shoulda', {
 
 Validator.add_rule('inclusion', {
   shoulda : function(value, pattern) {
-    if(!pattern.inclusion) {
+    if (!pattern.inclusion) {
       return false;
     }
 
     var flag = false;
-    for(var i in pattern.inclusion) {
-      if(pattern.inclusion[i] === value) {
+    if (value === "") {
+      return true;
+    }
+    for (var i in pattern.inclusion) {
+      if (pattern.inclusion[i] === value) {
         flag = true;
         break;
       }
@@ -191,13 +220,13 @@ Validator.add_rule('inclusion', {
 
 Validator.add_rule('exclusion', {
   shoulda : function(value, pattern) {
-    if(!pattern.exclusion) {
+    if (!pattern.exclusion) {
       return false;
     }
 
     var flag = true;
-    for(var i in pattern.exclusion) {
-      if(pattern.exclusion[i] === value) {
+    for (var i in pattern.exclusion) {
+      if (pattern.exclusion[i] === value) {
         flag = false;
         break;
       }
@@ -209,6 +238,9 @@ Validator.add_rule('exclusion', {
 
 Validator.add_rule('email', {
   shoulda : function(value, pattern) {
+    if (value === "") {
+
+    }
     var regex = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
     return regex.test(value);
   },
